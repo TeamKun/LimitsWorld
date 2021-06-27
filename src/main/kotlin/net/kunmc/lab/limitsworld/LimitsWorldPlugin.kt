72f4133e50e60937fe1f939cb9c2bc8c4ae0f991
stcYps <stcyps14@gmail.com>
2021-06-27 14:45:46 +0900

    [add]最小構成の作成

---
+++ src/main/kotlin/net/kunmc/lab/limitsworld/LimitsWorldPlugin.kt
@@ -0,0 +1,9 @@
+package net.kunmc.lab.limitsworld
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class LimitsWorldPlugin: JavaPlugin() {
+    override fun onEnable() {
+        logger.info("起動しました")
+    }
+}
